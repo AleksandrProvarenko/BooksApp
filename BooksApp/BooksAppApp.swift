7e454867a128118b33667cc58fad978a166dac29
AleksandrProvarenko <provar67@gmail.com>
2026-02-11 11:38:42 +0200

    chore: first commit

---
+++ BooksApp/BooksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksAppApp.swift
+//  BooksApp
+//
+//  Created by Alex Provarenko on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct BooksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
